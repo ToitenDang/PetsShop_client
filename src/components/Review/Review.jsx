a561ac58f2b53dd0d15ac39a35b46f9e63bd4f6f
ToitenDang <tkdang21@gmail.com>
2024-11-06 20:41:11 +0700

    Update review

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Typography, Button, TextField, Rating } from '@mui/material';
 //import { createNewReview, feachReviewByProductId } from "~/apis";
-import {ReviewFetch} from '~/REST-API-client/index'
+import { ReviewFetch } from '~/REST-API-client/index'
 
 
 const Review = ({ entityId, type }) => {
@@ -13,30 +13,37 @@ const Review = ({ entityId, type }) => {
     useEffect(() => {
         const fetchReviews = async () => {
             try {
-                
+
                 const review = await ReviewFetch.fetchReviewByEntityId(entityId, type);
-                setReview(review); 
+                setReview(review);
             } catch (error) {
-                
+
                 console.error('Error fetching review details:', error);
             }
         };
-    
-        
+
+
         fetchReviews();
     }, [entityId, type]);
 
     const handleSubmitReview = async (e) => {
         e.preventDefault();
-    
+
+        // const userId = localStorage.getItem("_id");
+        // const user = localStorage.getItem("name");
         const userId = "672890544296af5f67384a54"; // Thay thế bằng id thực tế trong localstorage
         const user = "Dang The Kyy"; // Thay thế bằng tên người dùng thực tế trong localstorage
-    
+
         if (!userId || !user) {
             console.error("Người dùng chưa đăng nhập");
             return; // Không cho phép gửi đánh giá nếu người dùng chưa đăng nhập
         }
-    
+
+        if(!rating || !comment){
+            alert("Bạn chưa nhập đánh giá!")
+            return;
+        }
+
         const newReview = {
             entityId,
             type,
@@ -46,14 +53,14 @@ const Review = ({ entityId, type }) => {
             comment,
             createdAt: new Date().toISOString()
         };
-    
+
         try {
             // Gọi API để tạo đánh giá mới
             const response = await ReviewFetch.createNewReview(newReview);
-    
+
             // Cập nhật danh sách đánh giá sau khi tạo thành công, sử dụng dữ liệu trả về từ API
-            setReview((prev) => [...prev, response.data]); 
-    
+            setReview((prev) => [...prev, response.data]);
+
             // Đặt lại giá trị rating và comment sau khi tạo đánh giá mới
             setRating(0);
             setComment('');
@@ -61,11 +68,11 @@ const Review = ({ entityId, type }) => {
         } catch (error) {
             console.error('Error creating review:', error);
         }
-    };    
-    
+    };
+
 
     return (
-        <Box 
+        <Box
             sx={{
                 marginTop: 4,
                 width: '100%',
@@ -74,7 +81,7 @@ const Review = ({ entityId, type }) => {
                 alignItems: 'center',
                 gap: 2
             }}>
-            <Box 
+            <Box
                 sx={{
                     width: { xs: '100%', md: '80%' },
                     border: (theme) => (theme.palette.mode === 'dark' ? '1px solid #fff' : '1px solid #000'),
@@ -101,7 +108,7 @@ const Review = ({ entityId, type }) => {
                         fullWidth
                         sx={{ marginTop: 1 }}
                     />
-                    <Button type="submit" variant="contained" onClick={handleSubmitReview} sx={{ marginTop: 2 }}>Gửi đánh giá</Button>
+                    <Button type="submit" variant="contained" sx={{ marginTop: 2 }}>Gửi đánh giá</Button>
                 </form>
             </Box>
 
@@ -116,24 +123,24 @@ const Review = ({ entityId, type }) => {
                 }}
             >
                 <Typography variant="h5">Đánh giá của người tiêu dùng</Typography>
-                {reviews && reviews.length === 0 
-                ? 
+                {reviews && reviews.length === 0
+                    ?
                     <Typography>Sản phẩm hiện chưa có đánh giá nào!</Typography>
-                : reviews.map(review => (
-                    <Box key={review?._id} sx={{ padding: 2, marginTop: 1 }}>
-                        <Box sx={{ display: 'flex', alignItems: 'center', gap: 1, mb: 1 }}>
-                            <Typography variant="body1"><strong>{review.user}</strong></Typography>
-                            <Rating
-                                name="rating"
-                                value={review?.rating}
-                                precision={0.5}
-                                size="small"
-                            />
+                    : reviews.map(review => (
+                        <Box key={review?._id} sx={{ padding: 2, marginTop: 1 }}>
+                            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1, mb: 1 }}>
+                                <Typography variant="body1"><strong>{review.user}</strong></Typography>
+                                <Rating
+                                    name="rating"
+                                    value={review?.rating}
+                                    precision={0.5}
+                                    size="small"
+                                />
+                            </Box>
+                            <Typography variant="body2"><strong>Bình luận:</strong> {review?.comment}</Typography>
+                            <Typography variant="body2" sx={{ fontStyle: 'italic' }}>{new Date(review.createdAt).toLocaleString()}</Typography>
                         </Box>
-                        <Typography variant="body2"><strong>Bình luận:</strong> {review?.comment}</Typography>
-                        <Typography variant="body2" sx={{ fontStyle: 'italic' }}>{new Date(review.createdAt).toLocaleString()}</Typography>
-                    </Box>
-                ))}
+                    ))}
             </Box>
         </Box>
     );
