f8511d29c2e6ce5a74c040837f8d2e57cacd3f1d
PhamHuuTuan21110713 <phamhuutuan20031@gmail.com>
2024-12-15 16:12:28 +0700

    re update

--- src/pages/Promotion/Promotion.jsx
+++ src/pages/Promotion/Promotion.jsx
@@ -13,17 +13,36 @@ import "react-toastify/dist/ReactToastify.css";
 const Promotion = () => {
     const [promotion, setPromotion] = useState();
     const { id } = useParams();
+    const [isvalid, setIsvalid] = useState(false);
     useEffect(() => {
         PromotionFetch.getById(id)
             .then(data => {
-                setPromotion(data.data);
+                const promotion = data.data;
+                if (promotion.state === true) {
+                    const today = new Date();
+                    const startOfToday = new Date(Date.UTC(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate(), 0, 0, 0, 0));
+                    const endOfToday = new Date(Date.UTC(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate(), 23, 59, 59, 999));
+                    const startDate = new Date(promotion.startDate);
+                    const endDate = new Date(promotion.endDate);
+                    const isPromotionValidToday =
+                        startDate <= endOfToday && endDate >= startOfToday;
+                    if (isPromotionValidToday) {
+                        setPromotion(data.data);
+                        setIsvalid(true)
+                    } else {
+                        setIsvalid(false);
+                    }
+                } else {
+                    setIsvalid(false);
+                }
+
             })
             .catch(err => {
                 toast.error(`Lỗi lấy dữ liệu chương trình khuyến mãi:`)
                 console.log(`Lỗi lấy dữ liệu chương trình khuyến mãi: ${err}`)
             })
     }, [id])
-    if (!promotion) {
+    if (!promotion && isvalid) {
         return (
             <Box sx={{ display: "flex", justifyContent: "center", marginTop: "150px" }}>
                 <CircularProgress />
@@ -31,40 +50,44 @@ const Promotion = () => {
         )
     }
     return (
-        <Box sx={{ marginTop: '150px', padding: "20px" }}>
-            <Box sx={{ display: "flex", alignItems: "center", flexDirection: "column", gap: 2 }}>
-                <Typography variant="h5" sx={{ fontWeight: "bold" }}>{promotion?.name}</Typography>
-                <Typography sx={{ fontSize: "1.2rem" }}>{promotion?.description}</Typography>
-            </Box>
-            <Box sx={{ marginTop: "40px" }}>
-                <Typography sx={{ fontSize: "1.2rem", textAlign: "center" }}>
-                    Chương trình kéo dài từ ngày <span style={{ color: "#f26843", fontWeight: "bold" }}>{new Date(promotion?.startDate).toLocaleDateString("vi-VN")} </span>
-                    cho đến đầu ngày <span style={{ color: "#f26843", fontWeight: "bold" }}>{new Date(promotion?.endDate).toLocaleDateString("vi-VN") }</span>
-                </Typography>
-            </Box>
-            <Divider sx={{ marginY: "20px" }} />
-            <Box>
-                <Box sx={{ display: "flex", justifyContent: "center" }}>
-                    <Box sx={{ padding: "20px", backgroundColor: "#f26843", width: "200px", display: "flex", justifyContent: "center", alignItems: "center", borderRadius: "10px" }}>
-                        <Typography sx={{ fontSize: "1.2rem", fontWeight: "bold", color: "#fff" }}>Sản phẩm</Typography>
-                    </Box>
+        isvalid ?
+            <Box sx={{ marginTop: '150px', padding: "20px" }}>
+                <Box sx={{ display: "flex", alignItems: "center", flexDirection: "column", gap: 2 }}>
+                    <Typography variant="h5" sx={{ fontWeight: "bold" }}>{promotion?.name}</Typography>
+                    <Typography sx={{ fontSize: "1.2rem" }}>{promotion?.description}</Typography>
                 </Box>
                 <Box sx={{ marginTop: "40px" }}>
-                    <Box sx={{ display: "flex", flexWrap: "wrap" }}>
-                        {
-                            promotion?.applicableProducts.map((prod, index) => {
-                                return (
-                                    <Box key={prod._id} className={myStyle.colProds}>
-                                        <ProductItem product={prod}/>
-                                    </Box>
-                                )
-                            })
-                        }
+                    <Typography sx={{ fontSize: "1.2rem", textAlign: "center" }}>
+                        Chương trình kéo dài từ ngày <span style={{ color: "#f26843", fontWeight: "bold" }}>{new Date(promotion?.startDate).toLocaleDateString("vi-VN", { timeZone: "UTC" })} </span>
+                        cho đến hết ngày <span style={{ color: "#f26843", fontWeight: "bold" }}>{new Date(promotion?.endDate).toLocaleDateString("vi-VN", { timeZone: "UTC" })}</span>
+                    </Typography>
+                </Box>
+                <Divider sx={{ marginY: "20px" }} />
+                <Box>
+                    <Box sx={{ display: "flex", justifyContent: "center" }}>
+                        <Box sx={{ padding: "20px", backgroundColor: "#f26843", width: "200px", display: "flex", justifyContent: "center", alignItems: "center", borderRadius: "10px" }}>
+                            <Typography sx={{ fontSize: "1.2rem", fontWeight: "bold", color: "#fff" }}>Sản phẩm</Typography>
+                        </Box>
+                    </Box>
+                    <Box sx={{ marginTop: "40px" }}>
+                        <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+                            {
+                                promotion?.applicableProducts.map((prod, index) => {
+                                    return (
+                                        <Box key={prod._id} className={myStyle.colProds}>
+                                            <ProductItem product={prod} />
+                                        </Box>
+                                    )
+                                })
+                            }
+                        </Box>
                     </Box>
                 </Box>
+                <ToastContainer />
+            </Box> :
+            <Box sx={{ marginTop: "150px" }}>
+                <Typography sx={{ textAlign: "center" }}>Khuyến mãi hết hạn hoặc không tồn tại</Typography>
             </Box>
-            <ToastContainer />
-        </Box>
     )
 }
 
